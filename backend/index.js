8cec0cdc13e5c5b41c573ca314399105f3cd8b52
ashutoshreddy66 <54981328+ashutoshreddy66@users.noreply.github.com>
2024-06-12 10:25:08 -0500

    changes made in index.js

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import { Book } from "./models/bookModel.js";
 const app = express();
 
 app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('MERN Stack Dev')
@@ -33,6 +34,18 @@ app.post('/books', async (request, response)=>{
         response.status(500).send({message : error.message});
     }
 });
+
+app.get('/books', async (request, response)=>{
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({count: books.length, data : books})
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message : error.message});
+    }
+});
+
+
 
 mongoose
     .connect(mongoDBURL)
