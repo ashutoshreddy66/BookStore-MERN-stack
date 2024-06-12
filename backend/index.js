6f2b0bbcf1a5c217a7844f1233595b934982b985
ashutoshreddy66 <54981328+ashutoshreddy66@users.noreply.github.com>
2024-06-12 10:32:17 -0500

    added functioanlity to fetch details based on id

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,17 @@ app.get('/books', async (request, response)=>{
     }
 });
 
+// get one book
+app.get('/books/:id', async (request, response)=>{
+    try {
+        const {id} = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message : error.message});
+    }
+});
 
 
 mongoose
